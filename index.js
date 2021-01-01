5427adcf48289896d0713b05b96375d531c8f4cb
Yassin Makrai <70712131+Makraiyassin@users.noreply.github.com>
2021-01-01 10:15:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.set("useFindAndModify", false);
 
 mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true,useNewUrlParser: true}, () => {
     console.log("Connected to db!");
-    app.listen(3000, () => console.log("Server Up and running"));
+    app.listen( process.env.PORT || 3000, () => console.log("Server Up and running"));
 });
 
 //view engine configuration
